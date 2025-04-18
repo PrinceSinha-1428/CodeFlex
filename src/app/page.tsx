79ec2a1d1ad433b5004059a0f45f7747a340804a
Prince <princesinha629@gmail.com>
2025-04-18 17:30:10 +0530

    Eslint warnings fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import TerminalOverlay from "@/components/TerminalOverlay";
 import { Button } from "@/components/ui/button";
 import UserPrograms from "@/components/UserPrograms";
 import { ArrowRightIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 
